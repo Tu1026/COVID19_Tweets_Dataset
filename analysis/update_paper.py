bc9311feef39ce15eb00be2259ac7c3af6198617
malo <vasum@lafayette.edu>
2020-04-11 13:57:58 -0400

    Completed Readme Automation using .Rmd

--- analysis/update_paper.py
+++ analysis/update_paper.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from shutil import copyfile
 import pandas as pd
 import datetime
@@ -10,6 +11,7 @@ data_directory = "analysis/until_2020_04_01/"
 # NOTE: IMPORANT: CODE ASSUMES THE STRUCTURE OF THE CSV FILES IS ALWAYS THE SAME
 # paper_directory is the location for the .tex files are, and the folders data/ and images/
 paper_directory = "analysis/paper/"
+keywords = ["coronavirus", "covid", "ncov19", "ncov2019", "virus"]
 number_of_languages = 14
 version = "v1.2"
 
@@ -38,7 +40,7 @@ def cleanTable1(tex_variables):
     Cleans Table 1 of paper.tex that displays the mean
      number of tweets, SD, and median on one day, for each month.
 
-    Reads the file analysis/until_2020_04_01/Tweets_Number_by_Month.csv
+    Reads the file analysis/until_[date]/Tweets_Number_by_Month.csv
     Writes to file analysis/paper/data/table1.csv
 
     Arguments:
@@ -49,12 +51,17 @@ def cleanTable1(tex_variables):
     table1.drop(table1.columns[0], axis=1, inplace=True)
     table1 = table1.rename(columns={"M": "Mean",  "Mdn": "Median"})
     table1 = table1.round(2)
-    tex_variables["tweetsNumber"] = table1["Total"][0]
+
+    # Store data for all the months
+    table1AllMonths = table1[table1["Month"] == "All_M"].reset_index()
+    tex_variables["tweetsNumber"] = table1AllMonths["Total"][0]
+    tex_variables["originalTweetsNumber"] = table1AllMonths["Total"][1]
+    tex_variables["retweetsNumber"] = table1AllMonths["Total"][2]
 
     # Now alter df to store as table1.csv
     # Uses only all tweets: not original or retweets
     table1 = table1[table1["Type"] == "All"]
-    # Does it for every month from Jan through December
+    # Does it for every month from Jan through DecemberX
     for i in range(1, 13):
         if (i < len(table1)):
             tex_variables["tweetsNumber" +
@@ -71,7 +78,7 @@ def cleanTable2(tex_variables):
     """
     Cleans Table 2 of paper.tex that displays languages upto number_of_languages, the number of tweets in that language, and its percent share.
 
-    Reads the file analysis/until_2020_04_01/TweetLanguages.csv
+    Reads the file analysis/until_[date]/TweetLanguages.csv
     Writes to file analysis/paper/data/table2.csv
 
     Arguments:
@@ -99,7 +106,7 @@ def cleanTable3(tex_variables):
     """
     Cleans Table 3 of paper.tex that displays summary statistics on number geolocated tweets, retweets, likes.
 
-    Reads the file analysis/until_2020_04_01/SummaryTable.csv
+    Reads the file analysis/until_[date]/SummaryTable.csv
     Writes to file analysis/paper/data/table3.csv
 
     Arguments:
@@ -111,7 +118,6 @@ def cleanTable3(tex_variables):
     table3 = table3.loc[2:len(table3) - 4]
     table3.to_csv(paper_directory
                   + "data/table3.csv", header=False, index=False, na_rep="0")
-    print(table3)
 
 
 def writeTexVariables(tex_variables):
@@ -129,9 +135,9 @@ def copyFigures():
     """
     Copies figures from the data_directory to paper_directory/images/
     Figures copied from:
-        analysis/until_2020_04_01/Tweets by Language.png
-        analysis/until_2020_04_01/Retweets.png
-        analysis/until_2020_04_01/GeoTweets.png
+        analysis/until_[date]/Tweets by Language.png
+        analysis/until_[date]/Retweets.png
+        analysis/until_[date]/GeoTweets.png
     Figures pasted in:
         analysis/paper/images/language.png
         analysis/paper/images/retweets.png
@@ -145,22 +151,86 @@ def copyFigures():
              paper_directory + "images/geotweets.png")
 
 
+def cleanTable4(tex_variables):
+    """Creates Number of tweets by Month table for readme.md
+
+    Reads the file analysis/until_[date]/Tweets_Number_by_Month.csv
+    Writes to file analysis/paper/data/table4.csv
+
+    Arguments:
+        tex_variables {string: string} -- Stored in variables.txt. paper.tex loads all these variables so that they can be accessed just like any regular latex command.
+    """
+    table4 = pd.read_csv(
+        data_directory + "Tweets_Number_by_Month.csv", engine='python')
+    table4.drop(table4.columns[0], axis=1, inplace=True)
+    table4 = table4.rename(columns={"M": "Mean",  "Mdn": "Median"})
+    table4 = table4.round(2)
+
+    # Now alter df to store as table1.csv
+    # Uses only all tweets: not original or retweets
+    table4 = table4[table4["Type"] == "All"]
+    # Does it for every month from Jan through December
+    table4output = pd.DataFrame({"Month": [table4["Month"][i] for i in range(
+        1, 13) if (i < len(table4))], "Number of Tweets": [0 for i in range(1, 13) if (i < len(table4))]})
+    for i in range(0, len(table4output)):
+        table4output["Number of Tweets"][i] = table4["Total"][i+1]
+
+    table4output.to_csv(paper_directory + "data/table4.csv",
+                        index=False, na_rep="0")
+
+
+def cleanTable5(tex_variables):
+    """Creates a tweets breakdown by search keyword table for readme.md
+
+    Reads the files analysis/until_[date]/[keyword]_Tweets_Number_by_Month.csv
+    Writes to file analysis/paper/data/table5.csv
+
+    Arguments:
+        tex_variables {string: string} -- Stored in variables.txt. paper.tex loads all these variables so that they can be accessed just like any regular latex command.
+    """
+    table5 = pd.DataFrame(
+        {"Keyword": keywords, "Number of Tweets": [0 for i in keywords]})
+    for i in range(len(keywords)):
+        keyword_df = pd.read_csv(
+            data_directory+keywords[i]+"_Tweets_Number_by_Month.csv", engine='python')
+        table5["Number of Tweets"][i] = keyword_df["Total"][0]
+
+    table5.to_csv(paper_directory + "data/table5.csv",
+                  index=False, na_rep="0")
+
+
 def main():
     """
     Sets up tex_variables. Cleans each table, writes the files, copies the figures, and compiles the paper pdf file.
     NOTE: IMPORANT: CODE ASSUMES THE STRUCTURE OF THE CSV FILES IS ALWAYS THE SAME
     NOTE: IMPORTANT: INSTALL ALL TEX PACKAGES FOR THIS CODE TO WORK. Ensure you have pdflatex. Install with MiKTeX or TeX Live.
+    NOTE: R libraries, "here", "knitr" required.
     NOTE: Paper will be stored in analysis/paper/paper.pdf
     """
     tex_variables = setUpTexVariables()
     cleanTable1(tex_variables)
     cleanTable2(tex_variables)
     cleanTable3(tex_variables)
+    cleanTable4(tex_variables)
+    cleanTable5(tex_variables)
     writeTexVariables(tex_variables)
     copyFigures()
 
+    # NOTE: R libraries, "here", "knitr", "rmarkdown" required.
+    subprocess.call(
+        ["Rscript", "-e", "rmarkdown::render('"+paper_directory+"readme.rmd', output_file='readme.md', params=list( \
+            version = '" + tex_variables['version'] +
+            "', date  = '" + tex_variables['date'] +
+            "', tweetsNumber = '" + str(tex_variables['tweetsNumber']) +
+            "',  originalTweetsNumber = '" + str(tex_variables['originalTweetsNumber']) +
+            "',  retweetsNumber = '" + str(tex_variables['retweetsNumber']) +
+            "'))"])
+
+    copyfile(paper_directory + "readme.md",
+             "README.md")
     # pdflatex paper.tex may produce many errors. These are due to missing LaTeX packages. Install each package used using tlmgr.
     # Installing required packages will resolve errors.
+
     os.chdir(os.path.dirname(paper_directory))
     os.system("pdflatex paper.tex")
 
